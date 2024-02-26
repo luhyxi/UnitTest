778998e9b585def7a70253d074ec7ea62307e8c8
luana <luhyxi@gmail.com>
2024-02-26 08:16:59 -0300

    tests: New null and expected results tests

---
+++ TestProject/Services/HotPotatoTests.cs
@@ -0,0 +1,46 @@
+﻿using UnitTest;
+
+namespace TestProject.Services
+{
+    public class TestesHotPotato
+    {
+
+        [Theory]
+        [InlineData(10)]
+        public void Retorna_ListaComPeloMenos_UmPerdedor(int jogadores)
+        {
+            // Arrange + Act
+            HotPotatoGame.Play(jogadores);
+            
+            // Assert
+            Assert.Single(HotPotatoGame.Perdedores);
+        }
+
+        [Theory]
+        [InlineData(10)]
+        public void Retorna_ResultadoNao_Nulo(int jogadores)
+        {
+            // Arrange
+            var NewQueue = new Queue<string>();
+            
+            // Act
+            HotPotatoGame.Play(jogadores, NewQueue, true);
+            
+            // Assert
+            Assert.NotNull(NewQueue);
+        }
+
+        [Theory]
+        [InlineData(10, 9)]
+        public void DeveTer_OMesmoNumeroDeJogadoresEsperados(int players, int esperados)
+        {
+            var playersQueue = new Queue<string>();
+
+            // Act
+            HotPotatoGame.Play(players, playersQueue);
+
+            // Assert
+            Assert.Equal(esperados, playersQueue.Count);
+        }
+    }
+}
