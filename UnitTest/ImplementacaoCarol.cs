0fb8e2e97cbb7cdc9bd81a77814c96b81b7f078b
luana <luhyxi@gmail.com>
2024-02-26 08:17:14 -0300

    feat: New logic

--- UnitTest/ImplementacaoCarol.cs
+++ UnitTest/ImplementacaoCarol.cs
@@ -2,29 +2,52 @@
 {
     public class HotPotatoGame
     {
-        public static int passesUntilExplode = 15;
+        public static int PassesUntilExplode = 15;
 
 
-        // TODO: Implementar formas não destrutivas de fazer tal método:
-        public static Queue<string> Play(int numberOfPlayers, Queue<string> playersQueue)
+        public static List<string> Perdedores = new List<string>();
+
+        public static Queue<string> Play(int numberOfPlayers, Queue<string> playersQueue, bool Destrutivo = true)
         {
+            InitializePlayersQueue(numberOfPlayers, playersQueue);
+
             Console.WriteLine($"Simulando jogo de batata quente com {numberOfPlayers} jogadores.");
-            Console.WriteLine($"A batata vai explodir após {passesUntilExplode} passes.");
+            Console.WriteLine($"A batata vai explodir após {PassesUntilExplode} passes.");
 
             return PerformPassesUntilExplode(playersQueue);
+        }
+        
+        public static Queue<string> Play(int numberOfPlayers, Queue<string> playersQueue)
+        {
+            Queue<string> NewQueue = playersQueue;
+                
+            InitializePlayersQueue(numberOfPlayers, NewQueue);
+
+            Console.WriteLine($"Simulando jogo de batata quente com {numberOfPlayers} jogadores.");
+            Console.WriteLine($"A batata vai explodir após {PassesUntilExplode} passes.");
+
+            return PerformPassesUntilExplode(NewQueue);
         }
 
         public static Queue<string> Play(int numberOfPlayers)
         {
-            var playersQueue = new Queue<string>();
-
+            var playersQueue = InitializePlayersQueue(numberOfPlayers);
+            
             Console.WriteLine($"Simulando jogo de batata quente com {numberOfPlayers} jogadores.");
-            Console.WriteLine($"A batata vai explodir após {passesUntilExplode} passes.");
+            Console.WriteLine($"A batata vai explodir após {PassesUntilExplode} passes.");
 
             return PerformPassesUntilExplode(playersQueue);
 
         }
 
+        public static void InitializePlayersQueue(int numberOfPlayers, Queue<string> queueToBeAdded)
+        {
+            for (int i = 1; i <= numberOfPlayers; i++)
+            {
+                queueToBeAdded.Enqueue($"Player {i}");
+            }
+        }
+        
         public static Queue<string> InitializePlayersQueue(int numberOfPlayers)
         {
             var playersQueue = new Queue<string>();
@@ -39,7 +62,7 @@
 
         public static Queue<string> PerformPassesUntilExplode(Queue<string> playersQueue)
         {
-            for (int passesCount = 1; passesCount <= passesUntilExplode; passesCount++)
+            for (int passesCount = 1; passesCount <= PassesUntilExplode; passesCount++)
             {
                 string currentPlayer = playersQueue.Dequeue();
                 Console.WriteLine($"A batata está passando por {currentPlayer} e esse é o passe {passesCount}.");
@@ -47,8 +70,9 @@
             }
 
             string eliminatedPlayer = playersQueue.Dequeue();
+            Perdedores.Add(eliminatedPlayer);
             Console.WriteLine($"A batata explodiu! {eliminatedPlayer} está fora do jogo.");
-
+            
             return playersQueue;
         }
     }
